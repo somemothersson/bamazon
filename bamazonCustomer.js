87b640423e221ba0a23453c23b3e9aaad02814a2
Stephen Malito <stevemalito@gmail.com>
2019-06-17 14:02:56 -0500

    bamazon manager now fully functioning

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -17,7 +17,7 @@ var connection = mysql.createConnection({
     database: "bamazon_db"
 });
 //array to hold stock items for ease of ordering insteading of showing ID
-var stockItems = [] 
+var stockItems = []
 queryStock()
 
 
@@ -31,11 +31,11 @@ function queryStock() {
             connection.query(`SELECT product_name 
             FROM bamazon_db.products
             `, function (err, product) {
-                for (i = 0; i <product.length; i++){
-                    stockItems.push(product[i].product_name)
-                }
-                // console.log(product)
-                // console.log(stockItems)
+                    for (i = 0; i < product.length; i++) {
+                        stockItems.push(product[i].product_name)
+                    }
+                    // console.log(product)
+                    // console.log(stockItems)
 
                 });
 
@@ -76,60 +76,43 @@ function bamStore() {
 //The first should ask them the ID of the product they would like to buy.
 function buyProduct() {
     inquirer
-        .prompt([{
-            name: "product",
-            type: "list",
-            message: "What would you like to buy?",
-            choices: stockItems
-        },
-        {
-            name: "quantity",
-            type: "input",
-            message: "How many Would you like to purchase?",
+    .prompt([{
+        name: "product",
+        type: "list",
+        message: "What would you like to buy?",
+        choices: stockItems
+    },
+    {
+        name: "quantity",
+        type: "input",
+        message: "How many Would you like to purchase?",
 
-        }])
-        .then((answer) => {
-            let item = answer.product;
-            let qty = answer.quantity;
-            let newStock = 0;
-            //Insufficient quantity!`, and then prevent the order from going through.
-            connection.query(`SELECT stock_quantity FROM bamazon_db.products
-            WHERE product_name = '${item}'`, function (err, response) {
-                    var stock = response[0].stock_quantity
-                    newStock = stock - qty
-                    // console.log(newStock)
-                    if (newStock < 0) {
-                        console.log("Insufficient Quantity")
-                        queryStock()
-                    } else if (newStock > 0) {
-                        connection.query(`UPDATE bamazon_db.products
-                        SET stock_quantity = '${newStock}' 
-                        WHERE product_name = '${item}'`, function (err, res) {
-                                if (err) throw err;
-                                connection.end();
-                            });
-                        console.log(`Your ${qty} ${item} are on the way!`)
+    }])
+    .then((answer) => {
+        let item = answer.product;
+        let qty = answer.quantity;
+        let newStock = 0;
+        //Insufficient quantity!`, and then prevent the order from going through.
+        connection.query(`SELECT stock_quantity FROM bamazon_db.products
+        WHERE product_name = '${item}'`, function (err, response) {
+            var stock = response[0].stock_quantity
+            newStock = stock - qty
+            // console.log(newStock)
+            if (newStock < 0) {
+                console.log("Insufficient Quantity")
+                queryStock()
+            } else if (newStock > 0) {
+                connection.query(`UPDATE bamazon_db.products
+                SET stock_quantity = '${newStock}' 
+                WHERE product_name = '${item}'`, function (err, res) {
+                        if (err) throw err;
+                        connection.end();
+                    });
+                console.log(`Your ${qty} ${item} are on the way!`)
 
-                    }
-                });
-
-
-
-
+            }
         });
 
+    });
+
 }
-
-
-//The app should then prompt users with two messages.
-
-//    * The first should ask them the ID of the product they would like to buy.
-//    * The second message should ask how many units of the product they would like to buy.
-
-// 7. Once the customer has placed the order, your application should check if your store has enough of the product to meet the customer's request.
-
-//    * If not, the app should log a phrase like `Insufficient quantity!`, and then prevent the order from going through.
-
-// 8. However, if your store _does_ have enough of the product, you should fulfill the customer's order.
-//    * This means updating the SQL database to reflect the remaining quantity.
-//    * Once the update goes through, show the customer the total cost of their purchase
--- bamazonManager.js
+++ bamazonManager.js
@@ -18,81 +18,89 @@ var connection = mysql.createConnection({
 });
 //array to hold stock items for ease of ordering insteading of showing ID
 var stockItems = []
+let departments = []
 buildStock()
 
 //builds the item array for easy updating via item name 
 function buildStock() {
-            connection.query(`SELECT product_name 
-            FROM bamazon_db.products
-            `, function (err, product) {
-                    for (i = 0; i < product.length; i++) {
-                        stockItems.push(product[i].product_name)
-                    }
-
-                });
-    setTimeout(bamManager, 1000);
+    connection.query(`SELECT product_name FROM bamazon_db.products
+        `, function (err, product) {
+        for (i = 0; i < product.length; i++) {
+            stockItems.push(product[i].product_name)
+        }
+        connection.query(`SELECT DISTINCT department_name
+         FROM bamazon_db.products;
+        `, function (err, department) {
+            for (i = 0; i < department.length; i++) {
+                departments.push(department[i].department_name)
+            }
+        });
+    });            
+    bamManager();
 }
 
 
 function bamManager() {
-    inquirer
-        .prompt({
-            name: "action",
-            type: "list",
-            message: "Bamazon Management Console",
-            choices: [
-                "Show Current Stock",
-                "Purchase Items",
-                "Add to Inventory",
-                "Add New Product",
-                "Exit"
-            ]
-        })
-        .then(function (answer) {
+            inquirer
+                .prompt({
+                    name: "action",
+                    type: "list",
+                    message: "Bamazon Management Console",
+                    choices: [
+                        "Show Current Stock",
+                        "View Low Inventory",
+                        "Add to Inventory",
+                        "Add New Product",
+                        "Exit"
+                    ]
+                })
+                .then(function (answer) {
 
-            switch (answer.action) {
-                case "Show Current Stock":
-                    queryStock();
-                    break;
-                case "View Low Inventory":
-                    lowStock()
-                    break;
-                case "Add to Inventory":
-                    updateStock()
-                    break;
-                case "Add New Product":
-                    addNewProduct()
-                    break;
-                case "Exit":
-                    connection.end();
-                    break;
-            }
-        });
-}
+                    switch (answer.action) {
+                        case "Show Current Stock":
+                            queryStock();
+                            break;
+                        case "View Low Inventory":
+                            lowStock()
+                            break;
+                        case "Add to Inventory":
+                            updateStock()
+                            break;
+                        case "Add New Product":
+                            addNewProduct()
+                            break;
+                        case "Exit":
+                            connection.end();
+                            break;
+                    }
+                });
+        }
 //show the current items and stock
 function queryStock() {
-    console.log("Current Stock")
-    connection.query(`SELECT * FROM bamazon_db.products
+            console.log("Current Stock")
+            connection.query(`SELECT * FROM bamazon_db.products
     `, function (err, response) {
-            if (err) throw err;
-            // Log all results of the SELECT statement
-            console.table(response);
-        });
-    
-}
+                    if (err) throw err;
+                    // Log all results of the SELECT statement
+                    console.table(response);
+                    bamManager()
+                });
+
+        }
 
 //show items that have quantities of less than 5
 function lowStock() {
-    console.log("Low Inventory Items")
-    connection.query(`SELECT * FROM bamazon_db.products
-    WHERE stock_quantity <5
+            console.log("Low Inventory Items")
+            connection.query(`SELECT * FROM bamazon_db.products
+    WHERE stock_quantity <15
     `, function (err, response) {
-            if (err) throw err;
-            // Log all results of the SELECT statement
-            console.table(response);
-        });
-    
-}
+                    if (err) throw err;
+                    // Log all results of the SELECT statement
+                    console.table(response);
+                    bamManager()
+                });
+
+        }
 //Update current stock
 function updateStock() {
     inquirer
@@ -105,80 +113,74 @@ function updateStock() {
         {
             name: "quantity",
             type: "input",
-            message: `What is the number of stock you need to add for ${answer.product}?`,
+            message: `What is the number of stock you need to add?`,
 
         }])
         .then((answer) => {
             let item = answer.product;
             let qty = answer.quantity;
-            let newStock = 0;
-            connection.query(`SELECT stock_quantity FROM bamazon_db.products
-            WHERE product_name = '${item}'`, function (err, response) {
-                    var stock = response[0].stock_quantity
-                    newStock = stock - qty
-                    // console.log(newStock)
-                    if (newStock < 0) {
-                        console.log("Insufficient Quantity")
-                        queryStock()
-                    } else if (newStock > 0) {
-                        connection.query(`UPDATE bamazon_db.products
-                        SET stock_quantity = '${newStock}' 
-                        WHERE product_name = '${item}'`, function (err, res) {
-                                if (err) throw err;
-                                connection.end();
-                            });
-                        console.log(`Your ${qty} ${item} are on the way!`)
-
-                    }
+            connection.query(`UPDATE bamazon_db.products
+            SET stock_quantity =  stock_quantity + '${qty}' 
+            WHERE product_name = '${item}'`, function (err, res) {
+                            if (err) throw err;
+                    
                 });
-
-
-
+            console.log(`You added ${qty} to ${item}`)
+            bamManager()
 
         });
+
 
 }
 function addNewProduct() {
     inquirer
         .prompt([{
             name: "product",
-            type: "list",
-            message: "What item do you need to update stock quanitites?",
-            choices: stockItems
+            type: "input",
+            message: "What is the Name of the new Product?",
         },
         {
+            name: "department",
+            type: "list",
+            message: `What department?`,
+            choices: departments
+
+        }, {
             name: "quantity",
             type: "input",
-            message: `what is the new value for ${answer.product}?`,
+            message: `What is the stock quantity?`,
 
+        }, {
+            name: "price",
+            type: "input",
+            message: `What is the price?`,
+        },{
+            name: "item_id",
+            type: "input",
+            message: `What is the item id?`,
         }])
         .then((answer) => {
+            let itemID = answer.item_id;
             let item = answer.product;
+            let department = answer.department
             let qty = answer.quantity;
-            let newStock = 0;
-            connection.query(`SELECT stock_quantity FROM bamazon_db.products
-            WHERE product_name = '${item}'`, function (err, response) {
-                    var stock = response[0].stock_quantity
-                    newStock = stock - qty
-                    // console.log(newStock)
-                    if (newStock < 0) {
-                        console.log("Insufficient Quantity")
-                        queryStock()
-                    } else if (newStock > 0) {
-                        connection.query(`UPDATE bamazon_db.products
-                        SET stock_quantity = '${newStock}' 
-                        WHERE product_name = '${item}'`, function (err, res) {
-                                if (err) throw err;
-                                connection.end();
-                            });
-                        console.log(`Your ${qty} ${item} are on the way!`)
-
-                    }
+            let price = answer.price;
+            connection.query(
+                `INSERT INTO bamazon_db.products 
+                (item_id, product_name, department_name, stock_quantity, price) 
+                VALUES 
+                ('${itemID}','${item}','${department}', '${qty}', '${price}')`
+                , function (err, res) {
+                    if (err) throw err;
                 });
-
-
-
-
+                console.log(`Item Added`)
+                bamManager()
         });
-
+    
+        
 }
+
+
+
+
+   
